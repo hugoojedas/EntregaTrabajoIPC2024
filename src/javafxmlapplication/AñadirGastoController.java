5ee86e5fcec3ee86ca143f76764dd1157f602d18
AlexPGG <garciapla27@gmail.com>
2024-05-25 17:48:03 +0200

    Proyecto IPC

---
+++ src/javafxmlapplication/AñadirGastoController.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author 34622
+ */
+public class AñadirGastoController implements Initializable {
+
+    @FXML
+    private Stage stage;
+    @FXML
+    private TextField importeField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField descripciónField;
+    @FXML
+    private DatePicker dateField;
+    
+    private boolean pulsadoOK = false;
+
+    /**
+     * Initializes the controller class.
+     */
+            
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void aceptar(ActionEvent event) {
+       
+       
+               
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+    }
+    
+}
